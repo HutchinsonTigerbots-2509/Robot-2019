0cac067f453aa4309dca487ec9288e1e8d6a91bc
GoldenGollem <20501643+GoldenGollem@users.noreply.github.com>
2019-01-23 09:44:42 -0600

    Removed @Depreciation tags

--- src/main/java/frc/robot/commands/DriveTurn.java
+++ src/main/java/frc/robot/commands/DriveTurn.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.command.Command;
  *We use this to turn in autonomous. You set a degree you want to turn (- is left and + is right) 
  *in the () when you call this command. This will set robot to turn that specific distance
  *
- * @deprecated
  * @author Robot 2018
  */
 public class DriveTurn extends Command {
