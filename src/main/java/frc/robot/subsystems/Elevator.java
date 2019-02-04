8cbe651402e15fbf224ba59c4b6e180af032590c
Tigerbots2509-Member <24500962+Tigerbots2509-Member@users.noreply.github.com>
2019-02-04 15:41:19 -0600

    Added Regions to Elevator.java

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -22,18 +22,30 @@ import frc.robot.Robot;
 import frc.robot.RobotMap;
 
 /**
- * Add your docs here.
+ * The Elevator Subsystem is where code that uses the lift mechanism
+ * is stored and can be accessed and used throughout the project
+ * 
+ * <h3> JavaDoc Categories for Functions: </h3>
+ * <li> + Lift Methods - Will use the lift mechanism
+ * <li> + Shifter - Will have something to do with the shifter for the spool masters
+ * <li> + Update Voids - Updates something, like data or sensors
+ * <li> + Elevator Getters - Will return a value or an object
+ * 
+ * @author DJ, Tony, Cole G, and Nate
  */
 public class Elevator extends Subsystem {
-
+  // #region IMPORT VARIBLES
+  // RobotMap Objects
   private final WPI_TalonSRX SpoolMaster = RobotMap.ElevatorMotorMaster;
   private final WPI_VictorSPX SpoolSlave = RobotMap.ElevatorMotorSlave;
   private final DoubleSolenoid mShifter = RobotMap.ElevatorShifter;
   private final DigitalInput mLeftLimit = RobotMap.ElevatorLeftLimit;
   private final DigitalInput mRightLimit = RobotMap.ElevatorRightLimit;
 
+  // ShuffleBoard Tab
   private final ShuffleboardTab mElevatorTab = Shuffleboard.getTab("Elevator");
 
+  // Constants
   private final double kPulseNumber = Constants.kPulsesPerRotation;
   private final double kMaxHeight = Constants.kMaxHieght;
   private final double kMidHeight = Constants.kMidHieght;
@@ -47,17 +59,21 @@ public class Elevator extends Subsystem {
   private final double ElevatorSensitivity = Constants.kElevatorSensitivity;
   private final double kTicksPerInch = Constants.kElevatorTicksPerInch;
 
+  // Pneumatics Values
   private final Value kReverse = Value.kReverse;
   private final Value kForward = Value.kForward;
   private final Value kHighGear = Value.kReverse;
   private final Value kLowGear = Value.kForward;
 
+  // Private PID Varibles
   private double mError;
   private double mPerpotional;
   private double mDerivative;
   private double mIntegral = 0;
   private double mPerviousError;
   private double mEncoderTargetHieght;
+
+  // #endregion IMPORT VARIBLES
 
   public Elevator() {
     setSubsystem("Elevator");
@@ -72,41 +88,15 @@ public class Elevator extends Subsystem {
   public void initDefaultCommand() {
   }
 
+   // #region Lift Methods
   /**
    * Stops both the Master and Slave motors
+   * 
+   * @category Lift Methods
    */
   public void StopMotors() {
     SpoolMaster.stopMotor();
     SpoolSlave.stopMotor();
-  }
-
-  /**
-   * Sets the SpoolMasters's enocder position to zero
-   */
-  public void ZeroSensor() {
-    SpoolMaster.setSelectedSensorPosition(0);
-  }
-
-  public double getTargetHeight(){
-    return mEncoderTargetHieght;
-  }
-
-  public void setTargetHeight(double height){
-    mEncoderTargetHieght = height;
-  }
-
-  /**
-   * Calculates PID Speed to send to the master
-   */
-  public double PIDFinal() {
-    mError = getTargetHeight() - CurrentHeight();
-    mPerpotional = mError * PGain;
-    mDerivative = (mError - mPerviousError) * DGain;
-    mIntegral += (mError * .02);
-    mPerviousError = mError;
-    UpdateTelemetry();
-    return (mPerpotional + mDerivative + (mIntegral * IGain));
-
   }
 
   /**
@@ -129,50 +119,8 @@ public class Elevator extends Subsystem {
     }
   }
 
-  /**
-   * Gets Current Height in encoder counts
-   */
-  public double CurrentHeight() {
-    return SpoolMaster.getSelectedSensorPosition();
-
-    // * ((kSpoolDiam * Math.PI) / kPulseNumber);
-    // return ElevatorEncoder.get()*((kSpoolDiam*Math.PI)/kPulseNumber);
-  }
-
-  /**
-   * This is similar to `ChaseTarget()` but instead uses the TalonSRX built in PID
-   * control loop.
-   * 
-   * @author Nate
-   */
-  public void setPosition(double targetInchesOffGround) {
-    double positionFromHome = targetInchesOffGround - kHomePositionInches;
-    double targetPositionRaw = positionFromHome * kTicksPerInch;
-    if (getLimitsValue() == false) {
-      SpoolMaster.set(ControlMode.Position, targetPositionRaw);
-    } else {
-      SpoolMaster.set(ControlMode.PercentOutput, 0);
-    }
-  }
-
-  /**
-   * This will return `true` if either the left or right limit switches return
-   * true. Or simply are triggered.
-   * 
-   * @return
-   */
-  public boolean getLimitsValue() {
-    return (mLeftLimit.get() || mRightLimit.get());
-  }
-
-  /**
-   * @author Nate
-   * @return Current Height in Inches
-   */
-  public double InchesOffGround() {
-    double currentRawPosition = SpoolMaster.getSelectedSensorPosition();
-    return (currentRawPosition / kTicksPerInch) + kHomePositionInches;
-  }
+  // #endregion Lift Methods
+  // #region Shifter
 
   /**
    * Shifts the Gear to High
@@ -225,6 +173,16 @@ public class Elevator extends Subsystem {
     }
   }
 
+  // #endregion Shifter
+  // #region Update Voids
+
+  /**
+   * Sets the SpoolMasters's enocder position to zero
+   */
+  public void ZeroSensor() {
+    SpoolMaster.setSelectedSensorPosition(0);
+  }
+
   /**
    * Updates the telemetry in the Elevator Subsystems to the Shuffleboard. Option
    * for the smartdashboard has been removed.
@@ -246,4 +204,76 @@ public class Elevator extends Subsystem {
     mElevatorTab.add("Integral", mIntegral);
     Shuffleboard.update();
   }
+
+  // #endregion Update Voids
+  // #region Elevator Getters and Setters
+
+  public double getTargetHeight(){
+    return mEncoderTargetHieght;
+  }
+
+  public void setTargetHeight(double height){
+    mEncoderTargetHieght = height;
+  }
+
+  /**
+   * Calculates PID Speed to send to the master
+   */
+  public double PIDFinal() {
+    mError = getTargetHeight() - CurrentHeight();
+    mPerpotional = mError * PGain;
+    mDerivative = (mError - mPerviousError) * DGain;
+    mIntegral += (mError * .02);
+    mPerviousError = mError;
+    UpdateTelemetry();
+    return (mPerpotional + mDerivative + (mIntegral * IGain));
+
+  }
+
+  /**
+   * Gets Current Height in encoder counts
+   */
+  public double CurrentHeight() {
+    return SpoolMaster.getSelectedSensorPosition();
+
+    // * ((kSpoolDiam * Math.PI) / kPulseNumber);
+    // return ElevatorEncoder.get()*((kSpoolDiam*Math.PI)/kPulseNumber);
+  }
+
+  /**
+   * This is similar to `ChaseTarget()` but instead uses the TalonSRX built in PID
+   * control loop.
+   * 
+   * @author Nate
+   */
+  public void setPosition(double targetInchesOffGround) {
+    double positionFromHome = targetInchesOffGround - kHomePositionInches;
+    double targetPositionRaw = positionFromHome * kTicksPerInch;
+    if (getLimitsValue() == false) {
+      SpoolMaster.set(ControlMode.Position, targetPositionRaw);
+    } else {
+      SpoolMaster.set(ControlMode.PercentOutput, 0);
+    }
+  }
+
+  /**
+   * This will return `true` if either the left or right limit switches return
+   * true. Or simply are triggered.
+   * 
+   * @return
+   */
+  public boolean getLimitsValue() {
+    return (mLeftLimit.get() || mRightLimit.get());
+  }
+
+  /**
+   * @author Nate
+   * @return Current Height in Inches
+   */
+  public double InchesOffGround() {
+    double currentRawPosition = SpoolMaster.getSelectedSensorPosition();
+    return (currentRawPosition / kTicksPerInch) + kHomePositionInches;
+  }
+
+  // #endregion
 }
