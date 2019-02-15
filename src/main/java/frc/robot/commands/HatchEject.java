6de29cd4886024c9e05908e171df42ec910cdb1c
Tigerbots2509-Member <24500962+Tigerbots2509-Member@users.noreply.github.com>
2019-02-15 10:48:53 -0600

    Wired Robot

--- src/main/java/frc/robot/commands/HatchEject.java
+++ src/main/java/frc/robot/commands/HatchEject.java
@@ -1,50 +1,50 @@
-package frc.robot.commands; // package declaration
+// package frc.robot.commands; // package declaration
 
-// imports
+// // imports
 
-import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
-import frc.robot.subsystems.Intake;
+// import edu.wpi.first.wpilibj.command.Command;
+// import frc.robot.Robot;
+// import frc.robot.subsystems.Intake;
 
-/**
- * Will eject the hatch by telling the pistons
- * for that to extend
- * 
- * Good as of 2/29/2019
- */
-public class HatchEject extends Command {
-  private Intake sIntake = Robot.sIntake;
-  public HatchEject() {
-    requires(sIntake);
-  }
+// /**
+//  * Will eject the hatch by telling the pistons
+//  * for that to extend
+//  * 
+//  * Good as of 2/29/2019
+//  */
+// public class HatchEject extends Command {
+//   private Intake sIntake = Robot.sIntake;
+//   public HatchEject() {
+//     requires(sIntake);
+//   }
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-    sIntake.HatchEject();
-  }
+//   // Called just before this Command runs the first time
+//   @Override
+//   protected void initialize() {
+//     sIntake.HatchEject();
+//   }
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    sIntake.HatchEject();
-  }
+//   // Called repeatedly when this Command is scheduled to run
+//   @Override
+//   protected void execute() {
+//     sIntake.HatchEject();
+//   }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+//   // Make this return true when this Command no longer needs to run execute()
+//   @Override
+//   protected boolean isFinished() {
+//     return false;
+//   }
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-    sIntake.EndAll();
-  }
+//   // Called once after isFinished returns true
+//   @Override
+//   protected void end() {
+//     sIntake.EndAll();
+//   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-  }
-}
+//   // Called when another command which requires one or more of the same
+//   // subsystems is scheduled to run
+//   @Override
+//   protected void interrupted() {
+//   }
+// }
